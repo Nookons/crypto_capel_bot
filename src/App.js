be0ab25350376b1d992deb1233fd6948ff10e2b9
Kolomiiets Dmytro <nookon@icloud.com>
2024-06-05 20:31:40 +0200

    add user nick

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Main from "./Display/Main";
 import Header from "./Header/Header";
 
 import './App.css'
+import {useTelegram} from "./Hooks/useTelegram";
 
 const App = () => {
+    const {user} = useTelegram();
+
     return (
         <div className={"container"}>
             <Header />
+            <h5>{user}</h5>
             <Main />
         </div>
     );
---
+++ src/Hooks/useTelegram.js
@@ -0,0 +1,9 @@
+const tg = window.Telegram.WebApp;
+
+export function useTelegram() {
+
+    return {
+        tg,
+        user: tg.initDataUnsafe?.user
+    }
+}
