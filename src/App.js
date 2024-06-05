143e09c973196744786d8fee4583770ee9b8df6c
Kolomiiets Dmytro <nookon@icloud.com>
2024-06-05 20:13:53 +0200

    remove all

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Header from "./Header/Header";
-import Main from "./Display/Main";
-import copyIcon from './Assets/copy.svg';
+import React from 'react';
 
 const App = () => {
-
     return (
-        <div className="container">
-            <Header />
-            <div className="centered-content">
-                <h5>test</h5>
-                <button
-                    style={{ border: "none", background: "none" }}
-                    aria-label="Copy"
-                >
-                    <img style={{ maxWidth: 20 }} src={copyIcon} alt="Copy Icon" />
-                </button>
-            </div>
-            <Main />
-            <button
-                className="close-button"
-                aria-label="Close App"
-            >
-                Close
-            </button>
+        <div>
+            test...
         </div>
     );
 };
