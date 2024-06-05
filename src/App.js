5bf888663d785131587aed24eef091d851b18ab4
Kolomiiets Dmytro <nookon@icloud.com>
2024-06-05 21:40:47 +0200

    add last test

--- src/App.js
+++ src/App.js
@@ -1,68 +1,69 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { doc, getDoc, updateDoc, setDoc } from 'firebase/firestore';
+
 import Main from "./Display/Main";
 import Header from "./Header/Header";
-
-import './App.css'
-import {useTelegram} from "./Hooks/useTelegram";
-import {Route, Routes} from "react-router-dom";
 import Posts from "./Posts/Posts";
+import { useTelegram } from "./Hooks/useTelegram";
+import { db } from "./firebase";
 
-import {doc, getDoc, updateDoc, setDoc} from "firebase/firestore";
-import {db} from "./firebase";
+import './App.css';
 
 const App = () => {
-    const {tg, user} = useTelegram();
+    const { tg, user } = useTelegram();
 
     useEffect(() => {
         tg.ready();
         tg.expand();
-    }, []);
+    }, [tg]);
 
     useEffect(() => {
         async function setUser() {
-            try {
-                if (user) {
-                    console.log(user)
-                    console.log("user_" + user.id)
+            if (user) {
+                console.log('User object:', user);
+                const userDocId = `user_${user.id}`;
+                console.log('User document ID:', userDocId);
 
-                    const docRef = doc(db, "users", user.id);
-                    const docSnap = await getDoc(docRef);
+                try {
+                    const userDocRef = doc(db, "users", userDocId);
+                    console.log('User document reference:', userDocRef);
+
+                    const docSnap = await getDoc(userDocRef);
+                    console.log('Document snapshot:', docSnap);
 
                     if (docSnap.exists()) {
-                        await updateDoc(doc(db, "users", "user_" + user.id), {
+                        await updateDoc(userDocRef, {
                             lastSignIn: Date.now(),
                         });
+                        console.log('User document updated');
                     } else {
-                        await setDoc(doc(db, "users", "user_" + user.id), {
+                        await setDoc(userDocRef, {
                             applicationId: Date.now(),
                             ...user
                         });
+                        console.log('New user document set');
                     }
+                } catch (error) {
+                    console.error("Error updating/setting user document:", error);
                 }
-            } catch (error) {
-                console.log(error);
             }
         }
 
         setUser();
-    }, [user, tg]);
-
+    }, [user]);
 
     return (
-        <div className={"container"}>
-            <Header/>
-            {user ?
-                <h5 style={{
-                    textAlign: "center",
-                    padding: 14
-                }}>
-                    Hello {user.first_name} {user.last_name} - {user.username}.tg
+        <div className="container">
+            <Header />
+            {user && (
+                <h5 style={{ textAlign: "center", padding: 14 }}>
+                    Hello {user.first_name} {user.last_name} - {user.username}
                 </h5>
-                : null
-            }
+            )}
             <Routes>
-                <Route index element={<Main/>}/>
-                <Route path={'posts'} element={<Posts/>}/>
+                <Route index element={<Main />} />
+                <Route path="posts" element={<Posts />} />
             </Routes>
         </div>
     );
