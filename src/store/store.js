bd51047d2a2e835dd2efcf48a4c30c79f7e84214
Kolomiiets Dmytro <nookon@icloud.com>
2024-07-07 07:22:37 +0200

    test

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,15 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { userReducer } from './reducers/userReducer';
+import { projectsReducer } from './reducers/projectsReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { thunk } from 'redux-thunk';
-import {projectsReducer} from "./reducers/projectsReducer"; // Note: `thunk` is a named import
+import thunk from 'redux-thunk'; // Import `redux-thunk` correctly as default
 
 const rootReducer = combineReducers({
     user: userReducer,
     projects: projectsReducer
 });
 
-const middleware = [thunk]; // Define middleware
+const middleware = [thunk]; // Define middleware correctly
 
 export const store = createStore(
     rootReducer,
